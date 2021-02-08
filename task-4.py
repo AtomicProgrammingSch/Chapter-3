ebe0a2fd0b81a2d09122cea1f42ab48f92a9f411
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-02-08 01:17:36 +0000

    finished task-4

---
+++ task-4.py
@@ -0,0 +1,5 @@
+print("Please enter your name:")
+name = input()
+print("Please enter your age:")
+age = int(input())  # I have made it an int so that further calculations could be done on it
+print("Hello there, " + name + "! You are " + str(age) + " years old!")
